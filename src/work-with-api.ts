1838218d6b7fa5a9bc2d4a498854d27cefbd12d0
Yerasyl04 <91834677+Yerasyl04@users.noreply.github.com>
2025-01-14 23:33:14 +0600

    Update work-with-api.ts

--- src/work-with-api.ts
+++ src/work-with-api.ts
@@ -3,16 +3,18 @@
 
 // API URL: https://jsonplaceholder.typicode.com/posts
 // Use axios library
+import axios from 'axios';
+
 type APIResponseType = {
-    id: number,
-    userId: number
-    title: string,
-    body: string,
-}
+    id: number;
+    userId: number;
+    title: string;
+    body: string;
+};
 
 async function fetchLongPosts(): Promise<APIResponseType[]> {
-    // Your code goes here
-    return []
+    const response = await axios.get<APIResponseType[]>('https://jsonplaceholder.typicode.com/posts');
+    return response.data.filter(post => post.body.length > 100);
 }
 
-module.exports = { fetchLongPosts }
+module.exports = { fetchLongPosts };
