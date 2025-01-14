9b6890a67bf661e6c2768d635e70b40c449800db
Yerasyl04 <91834677+Yerasyl04@users.noreply.github.com>
2025-01-14 18:59:49 +0600

    Update about-me.ts

--- src/about-me.ts
+++ src/about-me.ts
@@ -1,6 +1,16 @@
 export const contactInfo = {
-    name: 'Имя Фамилия', //Укажите здесь имя
-    phoneNumber: '+77713011111', //Укажите здесь свой номер телефона
-    email: 'yourmail@gmail.com', //Укажите здесь свой email
-    bio: '' //Напишите сопроводительное письмо(от 50 символов!!!)
+    name: 'Имя Фамилия', //Turysbek Yerassyl
+    phoneNumber: '+77713011111', //+77474784626
+    email: 'yourmail@gmail.com', //erasyl_turysbek@mail.ru
+    bio: '' //
+Dear AutoData Hr,
+My name is Erasyl, and I am a 4th-year student at KBTU, majoring in Information Systems.
+As part of an academic project, I led a startup where I worked as a frontend developer and designer.
+I have completed several courses, including Golang Development, Web Development, UI/UX Design, JavaScript and React,
+as well as "Technological Entrepreneurship and Startups" and Power BI. Additionally,
+I completed the "DataArt" QA Engineering course, where I learned all the responsibilities of a tester. 
+I also have a strong command of English, as I lived in the U.S. for 8 months.
+I am eager to apply my knowledge and experience to your project!
+Best regards,
+Yerassyl
 }  
